03bd67f0840de472378def4710e041c2af810e36
Nestor Mendoza <nestor.mendozam@gmail.com>
2023-03-13 17:05:48 -0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:delivery_app/src/pages/login/login_page.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: "Delivery Udemy",
+      initialRoute: "/",
+      getPages: [GetPage(name: "/", page: () => LoginPage())],
+      navigatorKey: Get.key,
+    );
+  }
+}
