152f249d83c0fc42e9bbd715b1572f8b472489e3
Nestor Mendoza <nestor.mendozam@gmail.com>
2023-03-13 19:09:07 -0600

    building login page

--- lib/src/pages/login/login_page.dart
+++ lib/src/pages/login/login_page.dart
@@ -6,7 +6,41 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('LOGIN PAGE')),
+      body: Stack(
+        // posicionar elementos unos encima de otros (layers)
+        children: [
+          _backgroundCover(),
+          Column(
+            // posicionar elementos uno debajo del otro
+            children: [
+              _imageCover(),
+              _textAppName(),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  // private methods
+  Widget _imageCover() {
+    return Container(
+      width: 130,
+      height: 130,
+      alignment: Alignment.center,
+      child: Image.asset("assets/images/delivery_icon.png"),
+    );
+  }
+
+  Widget _textAppName() {
+    return const Text("Delivery App");
+  }
+
+  Widget _backgroundCover() {
+    return Container(
+      width: double.infinity,
+      height: 200,
+      color: const Color.fromRGBO(237, 76, 46, 1),
     );
   }
 }
